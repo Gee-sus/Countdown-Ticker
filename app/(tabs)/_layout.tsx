7c421e40782592df68ed4d9cf7387023de9ff786
Gee-sus <geesingh77@hotmail.com>
2025-11-11 15:02:50 +1300

    chore: add layout scaffolding

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,26 @@
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function TabsLayout() {
+  return <Tabs
+   screenOptions={{
+    headerShown: true,
+   }}
+   
+  >
+
+    <Tabs.Screen name="home" options={{
+      title: "Home",
+      tabBarIcon: ({ color, size }) => (
+        <Ionicons name="home" color={color} size={size} />
+      ),
+    }} />
+
+    <Tabs.Screen name="ticker" options={{
+      title: "Ticker",
+      tabBarIcon: ({ color, size }) => (
+        <Ionicons name="timer" color={color} size={size} />
+      ),
+    }} />
+  </Tabs>
+}
